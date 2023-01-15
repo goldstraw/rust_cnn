effb79f21d39acd2525de6f582f695a6d126ccdc
Charlie Goldstraw <goldstrawcharlie@gmail.com>
2023-01-15 20:54:51 +0000

    Removed redundant size storage

--- cnn.rs
+++ cnn.rs
@@ -6,7 +6,7 @@ struct Kernel {
 }
 
 impl Kernel {
-    fn create_kernel(size: [usize; 3]) -> Kernel {
+    fn create_kernel(size: usize, depth: usize) -> Kernel {
         let val: Vec<Vec<Vec<f64>>> = vec![];
         let normal = Normal::new(0.0, 0.1).unwrap();
         for i in 0..size[2] {
@@ -29,34 +29,57 @@ impl Kernel {
 }
 
 struct ConvLayer {
-    input_size: [usize; 3],
+    input_size: usize,
+    input_depth: usize
     num_filters: usize,
-    kernel_size: [usize; 2],
-    output_size: [usize; 2],
+    kernel_size: usize,
+    output_size: usize,
     stride: usize,
     biases: Vec<f64>,
     learning_rate: f64,
 }
 
 impl ConvLayer {
-    fn create_conv_layer(input_size: [usize; 3], num_filters: usize, kernel_size: [usize; 2], stride: usize) -> ConvLayer {
+    fn create_conv_layer(input_size: usize, input_depth: usize, num_filters: usize, kernel_size: usize, stride: usize) -> ConvLayer {
 
         let mut biases: Vec<f64> = vec![];
         let mut kernels: Vec<Kernel> = vec![];
         let normal = Normal::new(0.0, 0.1).unwrap();
         for _ in 0..num_filters {
             biases.push(normal.sample(&mut rand::thread_rng()));
-            kernels.push(Kernel::create_kernel([kernel_size[0],kernel_size[1],input_size[2]]));
+            kernels.push(Kernel::create_kernel(kernel_size, input_depth));
         }
         
         let layer: ConvLayer = ConvLayer {
             input_size,
             num_filters,
             kernel_size,
-            output_size: [((input_size[0]-kernel_size[0]) / stride) + 1; 2],
+            output_size: [((input_size-kernel_size) / stride) + 1; 2],
             stride,
             biases,
             learning_rate: 0.01,
+        };
+
+        layer
+    }
+}
+
+struct MaxPoolingLayer {
+    input_size: usize,
+    input_depth: usize,
+    kernel_size: usize,
+    output_size: usize,
+    stride: usize,
+}
+
+impl MaxPoolingLayer {
+    fn create_mxpl_layer(input_size: usize, input_depth: usize, kernel_size: usize, stride: usize) -> MaxPoolingLayer {
+        let layer: MaxPoolingLayer = MaxPoolingLayer {
+            input_size,
+            input_depth,
+            kernel_size,
+            output_size: ((input_size[0]-kernel_size[0]) / stride) + 1,
+            stride,
         };
 
         layer
@@ -84,8 +107,8 @@ impl CNN {
         cnn
     }
     
-    fn add_conv_layer(&mut self, input_size: [usize; 3], num_filters: usize, kernel_size: [usize; 2], stride: usize) {
-        let mut layer: ConvLayer = ConvLayer::create_conv_layer(input_size, num_filters, kernel_size, stride);
+    fn add_conv_layer(&mut self, input_size: usize, input_depth: usize, num_filters: usize, kernel_size: usize, stride: usize) {
+        let mut layer: ConvLayer = ConvLayer::create_conv_layer(input_size, input_depth, num_filters, kernel_size, stride);
         self.layers.push(Layer::Conv(layer))
     }
 }
@@ -93,5 +116,5 @@ impl CNN {
 fn main() {
 
     let mut cnn: CNN = CNN::create_cnn();
-    cnn.add_conv_layer([28, 28, 3], 6, [5, 5], 1);
+    cnn.add_conv_layer(28, 3, 6, 5, 1);
 }
