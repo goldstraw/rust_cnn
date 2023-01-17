c47980338a8c668955b2375c2be17f36cb202090
Charlie Goldstraw <goldstrawcharlie@gmail.com>
2023-01-17 12:35:38 +0000

    Converted cnn to a cargo project.

---
+++ cnn/target/debug/cnn.d
@@ -0,0 +1,1 @@
+C:\Users\golds\Documents\Programs\goldML\cnn\target\debug\cnn.exe: C:\Users\golds\Documents\Programs\goldML\cnn\src\main.rs
---
+++ cnn/target/debug/deps/cnn-a39adabb499151d3.d
@@ -0,0 +1,5 @@
+c:\Users\golds\Documents\Programs\goldML\cnn\target\debug\deps\cnn-a39adabb499151d3.rmeta: src\main.rs
+
+c:\Users\golds\Documents\Programs\goldML\cnn\target\debug\deps\cnn-a39adabb499151d3.d: src\main.rs
+
+src\main.rs:
